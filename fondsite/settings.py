ae9c60c38985f41fd38f29ff86ec3d4909376159
marinemaks <maria.a.maksimova@gmail.com>
2018-02-24 22:19:41 +0300

    second commit

--- fondsite/settings.py
+++ fondsite/settings.py
@@ -98,8 +98,10 @@ WSGI_APPLICATION = 'fondsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+	'NAME': 'jion_db',
+	'USER': 'jion_admin',
+	'HOST':'localhost',
     }
 }
 
